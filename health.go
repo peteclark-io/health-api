9cdb6d7dd54a9f37221ffb75f566bd8548399a74
Clark, Peter <peter.clark@ft.com>
2017-01-12 15:30:22 +0000

    Adding health api

---
+++ health.go
@@ -0,0 +1,54 @@
+package health
+
+import "time"
+
+// CheckResult is the json rep of the check
+type CheckResult struct {
+	Name        string        `json:"name"`
+	Severity    int           `json:"severity"`
+	Ok          bool          `json:"ok"`
+	Description string        `json:"description"`
+	Impact      string        `json:"impact"`
+	CheckOutput string        `json:"checkOutput"`
+	Duration    time.Duration `json:"duration"`
+}
+
+// AggregateResult aggregates all health checks.
+type AggregateResult struct {
+	Application   string        `json:"application"`
+	Time          time.Time     `json:"time"`
+	OverallHealth bool          `json:"overallHealth"`
+	Results       []CheckResult `json:"results"`
+}
+
+// Aggregator runs all checks
+func Aggregator(checks ...func() CheckResult) func() interface{} {
+	return func() interface{} {
+		var results []CheckResult
+		overall := true
+		for _, check := range checks {
+			r := check()
+			results = append(results, r)
+			if !r.Ok {
+				overall = false
+			}
+		}
+
+		return AggregateResult{"fixtures-rw", time.Now(), overall, results}
+	}
+}
+
+// Ping does a basic connectivity check
+func Ping() func() CheckResult {
+	return func() CheckResult {
+		return CheckResult{
+			Name:        "ping",
+			Severity:    1,
+			Ok:          true,
+			Description: "Simple ping check.",
+			Impact:      "No impact.",
+			CheckOutput: "pong",
+			Duration:    time.Millisecond * 0,
+		}
+	}
+}
